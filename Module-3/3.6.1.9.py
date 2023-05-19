e1b9aef7b705ee1345ba1240fd14ee18275e5b54
twadolowski <t.wadolowski@icloud.com>
2023-05-19 11:52:05 +0200

    Better answer 3.6.1.9

--- Module-3/3.6.1.9.py
+++ Module-3/3.6.1.9.py
@@ -1,9 +1,8 @@
 my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
 new_list = []
-
-for i in my_list:
-    if i not in new_list:
-        new_list.append(i)
-
+for number in my_list:  # Browse all numbers from the source list.
+    if number not in new_list:  # If the number doesn't appear within the new list...
+        new_list.append(number)  # ...append it here.
+my_list = new_list[:]  # Make a copy of new_list.
 print("The list with unique elements only:")
-print(new_list)
+print(my_list)
