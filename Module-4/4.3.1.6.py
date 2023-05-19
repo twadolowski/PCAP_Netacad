5cf4e66aad69910e2fd1c8cfdd8cb780128a01b1
twadolowski <t.wadolowski@icloud.com>
2023-05-19 19:04:08 +0200

    elif approach

--- Module-4/4.3.1.6.py
+++ Module-4/4.3.1.6.py
@@ -1,19 +1,19 @@
 def is_year_leap(year):
-    leap = False
-    if year % 4 == 0:
-        leap = True
-        if year % 100 == 0:
-            leap = False
-            if year % 400 == 0:
-                leap = True
-    return leap
+    if year % 4 != 0:
+        return False
+    elif year % 100 != 0:
+        return True
+    elif year % 400 != 0:
+        return False
+    else:
+        return True
 
 
 test_data = [1900, 2000, 2016, 1987]
 test_results = [False, True, True, False]
 for i in range(len(test_data)):
     yr = test_data[i]
-    print(yr, "->", end="")
+    print(yr, "-> ", end="")
     result = is_year_leap(yr)
     if result == test_results[i]:
         print("OK")
