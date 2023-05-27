2081fe8ba13e9761d20b6f3f3ad7f9f67b2c41ef
twadolowski <t.wadolowski@icloud.com>
2023-05-27 20:03:14 +0200

    Task 3.2.1.15

---
+++ Module-7/3.2.1.15.py
@@ -0,0 +1,26 @@
+class QueueError(IndexError):
+    pass
+
+
+class Queue:
+    def __init__(self):
+        self.__queue = []
+
+    def put(self, elem):
+        self.__queue.append(elem)
+
+    def get(self):
+        if len(self.__queue) == 0:
+            raise QueueError
+        return self.__queue.pop(0)
+
+
+que = Queue()
+que.put(1)
+que.put("dog")
+que.put(False)
+try:
+    for i in range(4):
+        print(que.get())
+except QueueError:
+    print("Queue error")
